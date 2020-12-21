70a6af96371c9cca3645744c573016c4c126b805
Fernando Eduardo da Silva <aironou@aironou.com.br>
2020-12-20 22:21:53 -0300

    Fix exiting after install oh-my-zsh

--- bin/init.sh
+++ bin/init.sh
@@ -27,7 +27,7 @@ clone_dotfiles() {
 
 install_oh_my_zsh() {
     log ">>> installing oh-my-zsh"
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     log ">>> installing powerlevel10k"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
